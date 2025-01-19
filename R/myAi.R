b8dbb00405c611fa0725535dd2ecde13a3ec85f7
gali1998 <gali1998@gmail.com>
2025-01-19 13:56:00 +0100

    Add files via upload

---
+++ R/myAi.R
@@ -0,0 +1,37 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+#' @export
+ask_gpt<- function(api_key, message = "", model ="gpt-4o-mini") {
+  messages = list(
+    list(
+      "role" = "user",
+      "content" = message
+    )
+  )
+  res <- openai::create_chat_completion(openai_api_key = api_key, model= model, messages = messages)
+  return(res$choices$message.content)
+
+}
+
+#' @export
+ask_claude <- function(api_key, message = "", model = "claude-2") {
+  response <- claudeR(prompt = "What is the capital of France?", model = model, api_key = key)
+  return(response)
+}
